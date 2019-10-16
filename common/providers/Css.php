25dcb15e9870df1adc14b2124e4dc6967f2ca45a
Ian <mypetertw@gmail.com>
2019-10-16 18:21:11 +0800

    FIX

--- common/providers/Css.php
+++ common/providers/Css.php
@@ -3,7 +3,8 @@
 | NOTE: CUSTOM LOAD
 */
 $CUSTOM_CSS = [
-    'vendor/swiper-4.5.0/dist/css/swiper.css',
+    // 'vendor/swiper-4.5.0/dist/css/swiper.css',
+    // 'vendor/fancybox-2.1.7/source/jquery.fancybox.css',
 ];
 foreach ($CUSTOM_CSS as $key) {
     echo Helper\Css::REQUIRE($key);
--- common/providers/Js.php
+++ common/providers/Js.php
@@ -6,9 +6,10 @@ $CUSTOM_JS = [
     'common/js/jquery-3.4.1.min.js',
     'common/js/jquery-ui-1.12.1.min.js',
     'common/js/hashtag.min.js',
-    'vendor/swiper-4.5.0/dist/js/swiper.min.js',
-    'vendor/clipboard.js-master/dist/clipboard.min.js',
+    // 'vendor/swiper-4.5.0/dist/js/swiper.min.js',
+    // 'vendor/clipboard.js-master/dist/clipboard.min.js',
     // 'vendor/Croppie-master/croppie.min.js',
+    // 'vendor/fancybox-2.1.7/source/jquery.fancybox.pack.js',
 ];
 foreach ($CUSTOM_JS as $key) {
     echo Helper\Js::REQUIRE($key);
