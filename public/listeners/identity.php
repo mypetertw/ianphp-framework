3180faf49373f0f8b52b0a0bbc965a05828d819c
Ian <mypetertw@gmail.com>
2019-10-17 12:06:07 +0800

    FIX

--- public/listeners/identity.php
+++ public/listeners/identity.php
@@ -1,7 +1,4 @@
 <?
-/*
-| NOTE: ***IMPORTANT*** DO NOT EDIT THOSE VALUES ***IMPORTANT***
-*/
 if (ROUTER == '/templates/signin' && $_SESSION['user-id']) {
     exit (Server\Redirect::LOCATION('/'));
 }
