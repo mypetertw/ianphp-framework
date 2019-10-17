8bd043ec2c2a365f929533dd9e6cd048086c4bcc
Ian <mypetertw@gmail.com>
2019-10-18 00:39:30 +0800

    FIX

---
+++ common/providers/BasicLayout.php
@@ -0,0 +1,12 @@
+<?
+$BASIC_LAYOUT = [
+    '<div id="gotop">↑</div>',
+    '<div class="display-none notification-message"></div>',
+    '<div class="load"></div>',
+    '<div class="progress"></div>',
+    '<div class="display-none page-loading-layout page-loading-layout-evt"><div class="page-loading-icon translate-middle"><img src="/images/loading.svg"></div></div>',
+];
+
+foreach ($BASIC_LAYOUT as $key) {
+    echo $key;
+}
---
+++ common/providers/CustomLayout.php
@@ -0,0 +1,8 @@
+<?
+/**
+ * CUSTOM LAYOUTS
+ */
+$LINE_30 = '<div class="line top-bottom-30"></div>';
+$LINE_20 = '<div class="line top-bottom-20"></div>';
+$LINE_10 = '<div class="line top-bottom-10"></div>';
+$LINE = '<div class="line"></div>';
