85c7ac60e68165d975b2027366975c618cacb38e
Ian <mypetertw@gmail.com>
2019-10-18 10:30:17 +0800

    FIX

--- public/handlers/simple.php
+++ public/handlers/simple.php
@@ -1,20 +1,10 @@
 <?
-/*
-| NOTE: ***IMPORTANT*** DO NOT EDIT THOSE VALUES ***IMPORTANT***
-*/
 require_once __DIR__ . '/../root.php';
 require_once ROOT . '/common/backend.php';
 
-/*
-| NOTE: HANDLER VERIFICATION
-*/
-if (HANDLER != 'XXX') {
-    exit (Server\Response::FAILED('BAD_HANDLER', true, HANDLER, NOW_SELF, 'error-msg'));
-}
-
-/*
-| NOTE: DO QUERY
-*/
+/**
+ * START CODING
+ */
 try {
     $PDO->beginTransaction();
 
@@ -34,7 +24,7 @@ try {
     exit (Server\Response::FAILED($e->getMessage(), true, HANDLER, NOW_SELF, 'error-msg'));
 }
 
-/*
-| NOTE: SUCCESS RESPONSE 200
-*/
+/**
+ * @return code 200
+ */
 exit (Server\Response::SUCCESS());
